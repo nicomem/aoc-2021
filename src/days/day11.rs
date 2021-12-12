641cc40a74f571c0d0c300c1592d446f1e37fea6
Nicolas Mémeint <nicomem@pm.me>
2021-12-12 09:34:13 +0100

    Impl day 11

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,24 +1,197 @@
+use std::ops::{Index, IndexMut};
+
 use crate::Solution;
 
 pub struct Day11;
 
-impl Solution for Day11 {
-    /// TODO
-    fn q1(&self, data: &str) -> String {
-        let _lines = Self::parse_data(data);
-        todo!()
+#[derive(Clone)]
+struct Grid<T> {
+    data: Vec<T>,
+    width: usize,
+    height: usize,
+}
+
+type YX = (usize, usize);
+
+impl<T> Grid<T> {
+    /// Try to get a cell data. Returns None if invalid.
+    fn get(&self, (y, x): YX) -> Option<&T> {
+        if x < self.width && y < self.height {
+            self.data.get(x + y * self.width)
+        } else {
+            None
+        }
     }
 
-    /// TODO
+    /// Try to get a cell data. Returns None if invalid.
+    fn get_mut(&mut self, (y, x): YX) -> Option<&mut T> {
+        if x < self.width && y < self.height {
+            self.data.get_mut(x + y * self.width)
+        } else {
+            None
+        }
+    }
+
+    fn left(&self, (y, x): YX) -> Option<YX> {
+        (x > 0).then(|| (y, x - 1))
+    }
+
+    fn right(&self, (y, x): YX) -> Option<YX> {
+        (x + 1 < self.width).then(|| (y, x + 1))
+    }
+
+    fn top(&self, (y, x): YX) -> Option<YX> {
+        (y > 0).then(|| (y - 1, x))
+    }
+
+    fn bottom(&self, (y, x): YX) -> Option<YX> {
+        (y + 1 < self.height).then(|| (y + 1, x))
+    }
+
+    /// Create an iterator over the (y, x) coordinates.
+    fn coordinates(&self) -> impl Iterator<Item = YX> + '_ {
+        (0..self.height).flat_map(|y| (0..self.width).map(move |x| (y, x)))
+    }
+}
+
+impl Grid<u8> {
+    /// Increment the cell.
+    /// Then if it flashes (== 10), push it to the stack.
+    fn inc_cell(coord: YX, buf: &mut Grid<u8>, stack: &mut Vec<YX>) {
+        let cell = &mut buf[coord];
+        *cell += 1;
+        if *cell == 10 {
+            stack.push(coord);
+        }
+    }
+
+    /// Run a tick of the dumbo octopus simulation,
+    /// returning the number of flashes during that tick.
+    /// The buffer must have the same size as this grid.
+    fn run_tick(&mut self, buf: &mut Self) -> u64 {
+        assert_eq!(self.width, buf.width);
+        assert_eq!(self.height, buf.height);
+
+        let mut stack = Vec::with_capacity(8);
+
+        // Copy and increment all by 1
+        buf.data.copy_from_slice(&self.data);
+        self.coordinates()
+            .for_each(|p| Self::inc_cell(p, buf, &mut stack));
+
+        let mut flashes = 0;
+        while let Some(coord) = stack.pop() {
+            flashes += 1;
+
+            // Top
+            if let Some(top) = self.top(coord) {
+                if let Some(topleft) = self.left(top) {
+                    Self::inc_cell(topleft, buf, &mut stack);
+                }
+                Self::inc_cell(top, buf, &mut stack);
+                if let Some(topright) = self.right(top) {
+                    Self::inc_cell(topright, buf, &mut stack);
+                }
+            }
+
+            // Left/Right
+            if let Some(left) = self.left(coord) {
+                Self::inc_cell(left, buf, &mut stack);
+            }
+            if let Some(right) = self.right(coord) {
+                Self::inc_cell(right, buf, &mut stack);
+            }
+
+            // Bottom
+            if let Some(bottom) = self.bottom(coord) {
+                if let Some(bottomleft) = self.left(bottom) {
+                    Self::inc_cell(bottomleft, buf, &mut stack);
+                }
+                Self::inc_cell(bottom, buf, &mut stack);
+                if let Some(bottomright) = self.right(bottom) {
+                    Self::inc_cell(bottomright, buf, &mut stack);
+                }
+            }
+        }
+
+        // Copy back buf into self, and make flashed cells go back to 0
+        self.data.copy_from_slice(&buf.data);
+        self.data
+            .iter_mut()
+            .filter(|c| **c >= 10)
+            .for_each(|c| *c = 0);
+
+        flashes
+    }
+}
+
+impl<T> Index<YX> for Grid<T> {
+    type Output = T;
+
+    fn index(&self, idx: YX) -> &Self::Output {
+        self.get(idx).unwrap()
+    }
+}
+
+impl<T> IndexMut<YX> for Grid<T> {
+    fn index_mut(&mut self, idx: YX) -> &mut Self::Output {
+        self.get_mut(idx).unwrap()
+    }
+}
+
+impl Solution for Day11 {
+    /// Run 100 steps of the octopuses simulation.
+    /// Count the number of flashes.
+    fn q1(&self, data: &str) -> String {
+        let mut grid = Self::parse_data(data);
+        let mut buf = grid.clone();
+        let mut flashes = 0;
+
+        for _step in 0..100 {
+            flashes += grid.run_tick(&mut buf);
+        }
+
+        flashes.to_string()
+    }
+
+    /// Find the first step where all octopuses flashes together.
     fn q2(&self, data: &str) -> String {
-        let _lines = Self::parse_data(data);
-        todo!()
+        let mut grid = Self::parse_data(data);
+        let mut buf = grid.clone();
+
+        for step in 1u32.. {
+            grid.run_tick(&mut buf);
+
+            if grid.data.iter().all(|c| *c == 0) {
+                // All flashes together
+                return step.to_string();
+            }
+        }
+
+        unreachable!()
     }
 }
 
 impl Day11 {
-    /// TODO
-    fn parse_data(data: &str) -> impl Iterator<Item = &str> + '_ {
-        data.split_terminator('\n')
+    /// Parse the grid of digits
+    fn parse_data(data: &str) -> Grid<u8> {
+        let mut lines = data.split_terminator('\n').map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).expect("Not digit") as u8)
+        });
+
+        // Extract the first line to get the width
+        let mut data = lines.next().unwrap().collect::<Vec<_>>();
+        let width = data.len();
+
+        // Extract the rest of the grid
+        data.extend(lines.flatten());
+        let height = data.len() / width;
+
+        Grid {
+            data,
+            width,
+            height,
+        }
     }
 }
--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,24 +1,196 @@
+use itertools::Itertools;
+
 use crate::Solution;
 
 pub struct Day12;
 
-impl Solution for Day12 {
-    /// TODO
-    fn q1(&self, data: &str) -> String {
-        let _lines = Self::parse_data(data);
-        todo!()
+type Room = usize;
+
+struct CaveMap {
+    /// Number of rooms in the cave
+    rooms: Room,
+
+    /// Whether each room is big or small
+    small_rooms: Vec<bool>,
+
+    /// The rooms one can go from another
+    paths: Vec<Vec<Room>>,
+
+    /// The names of the rooms
+    names: Vec<String>,
+
+    /// The starting room
+    start: Room,
+
+    /// The ending room
+    end: Room,
+}
+
+impl CaveMap {
+    /// Try to find a room index by its name.
+    /// If it does not exist, insert it and return its index.
+    fn find_or_push_room(&mut self, name: &str) -> Room {
+        if let Some(idx) = self
+            .names
+            .iter()
+            .find_position(|&s| s == name)
+            .map(|(i, _)| i)
+        {
+            idx as _
+        } else {
+            self.rooms += 1;
+            self.names.push(name.to_string());
+            self.small_rooms
+                .push(name.chars().all(|c| c.is_lowercase()));
+            self.paths.push(vec![]);
+
+            match name {
+                "start" => self.start = self.rooms - 1,
+                "end" => self.end = self.rooms - 1,
+                _ => {}
+            }
+
+            self.rooms - 1
+        }
     }
 
-    /// TODO
+    /// Visit the cave from a room.
+    /// At each path, will call `advance` which must return whether to advance to this room or not.
+    /// If advancing, do the same for all paths in this room.
+    /// After having advanced and processed a room, `go_back` will be called.
+    fn visit<FADV, FBACK, BAG>(
+        &self,
+        from: Room,
+        advance: &mut FADV,
+        go_back: &mut FBACK,
+        travel_bag: &mut BAG,
+    ) where
+        FADV: FnMut(Room, Room, &mut BAG) -> bool,
+        FBACK: FnMut(Room, Room, &mut BAG),
+    {
+        let paths = &self.paths[from];
+        for &to in paths {
+            if advance(from, to, travel_bag) {
+                self.visit(to, advance, go_back, travel_bag);
+                go_back(to, from, travel_bag);
+            }
+        }
+    }
+}
+
+impl Solution for Day12 {
+    /// Find the number of unique paths from start to end.
+    fn q1(&self, data: &str) -> String {
+        let map = Self::parse_data(data);
+
+        let mut npaths = 0u64;
+        let mut visited = vec![false; map.rooms];
+        visited[map.start] = true;
+
+        let mut advance = |_from, to, visited: &mut Vec<bool>| {
+            // If advancing goes to the end, mark the path directly without advancing
+            if to == map.end {
+                npaths += 1;
+                return false;
+            }
+
+            // Cannot advance twice to the same small room
+            if map.small_rooms[to] && visited[to] {
+                return false;
+            }
+
+            // Advance and update the path
+            visited[to] = true;
+            true
+        };
+
+        let mut go_back = |from, _to, visited: &mut Vec<bool>| {
+            visited[from] = false;
+        };
+
+        map.visit(map.start, &mut advance, &mut go_back, &mut visited);
+        npaths.to_string()
+    }
+
+    /// Same as q1 but can visit a single small cave twice (expect start & end)
     fn q2(&self, data: &str) -> String {
-        let _lines = Self::parse_data(data);
-        todo!()
+        let map = Self::parse_data(data);
+        let mut npaths = 0u64;
+
+        let mut advance = |_from, to, (visited, visited_twice): &mut (Vec<bool>, Option<Room>)| {
+            // If advancing goes to the end, mark the path directly without advancing
+            if to == map.end {
+                npaths += 1;
+                return false;
+            }
+
+            // Cannot advance twice to the same small room
+            if map.small_rooms[to] && visited[to] {
+                // If can visit twice this small room, do it
+                if visited_twice.is_none() && (to != map.start && to != map.end) {
+                    *visited_twice = Some(to);
+                    return true;
+                }
+                return false;
+            }
+
+            // Advance and update the path
+            visited[to] = true;
+            true
+        };
+
+        let mut go_back = |from, _to, (visited, visited_twice): &mut (Vec<bool>, Option<Room>)| {
+            if let Some(room) = visited_twice {
+                if *room == from {
+                    *visited_twice = None;
+                    return;
+                }
+            }
+
+            visited[from] = false;
+        };
+
+        let visited_twice: Option<Room> = None;
+        let mut visited = vec![false; map.rooms];
+        visited[map.start] = true;
+
+        map.visit(
+            map.start,
+            &mut advance,
+            &mut go_back,
+            &mut (visited, visited_twice),
+        );
+        npaths.to_string()
     }
 }
 
 impl Day12 {
-    /// TODO
-    fn parse_data(data: &str) -> impl Iterator<Item = &str> + '_ {
-        data.split_terminator('\n')
+    /// Parse the cave map
+    fn parse_data(data: &str) -> CaveMap {
+        let mut cave_map = CaveMap {
+            rooms: 0,
+            small_rooms: vec![],
+            paths: vec![],
+            names: vec![],
+            start: 0,
+            end: 0,
+        };
+
+        for (from, to) in data
+            .split_terminator('\n')
+            .map(|line| line.split_once('-').unwrap())
+        {
+            let from = cave_map.find_or_push_room(from);
+            let to = cave_map.find_or_push_room(to);
+
+            cave_map.paths[from].push(to);
+            cave_map.paths[to].push(from);
+        }
+
+        for path in &mut cave_map.paths {
+            path.sort_unstable();
+        }
+
+        cave_map
     }
 }
