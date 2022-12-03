b36418c1a956bf4e9e7a0d059b5ab3787e2779e8
Nicolas Mémeint <nicomem@pm.me>
2022-12-03 11:17:21 +0100

    2022 - 03 / 04

--- src/y2022/day2.rs
+++ src/y2022/day2.rs
@@ -1,13 +1,166 @@
+use std::{cmp::Ordering, str::FromStr};
+
 use crate::Solution;
 
 pub struct Day2;
 
 impl Solution for Day2 {
-    fn q1(&self, _data: &str) -> String {
-        todo!()
+    fn q1(&self, data: &str) -> String {
+        let rounds = parse1(data);
+        rounds
+            .map(|round| round.score() as u64)
+            .sum::<u64>()
+            .to_string()
     }
 
-    fn q2(&self, _data: &str) -> String {
-        todo!()
+    fn q2(&self, data: &str) -> String {
+        let rounds = parse2(data);
+        rounds
+            .map(|round| round.score() as u64)
+            .sum::<u64>()
+            .to_string()
+    }
+}
+
+fn parse1(data: &str) -> impl Iterator<Item = Round> + '_ {
+    data.split('\n').flat_map(|line| line.parse().ok())
+}
+
+fn parse2(data: &str) -> impl Iterator<Item = Round2> + '_ {
+    data.split('\n').flat_map(|line| line.parse().ok())
+}
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum Choice {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Choice {
+    fn score(self) -> u8 {
+        match self {
+            Choice::Rock => 1,
+            Choice::Paper => 2,
+            Choice::Scissors => 3,
+        }
+    }
+
+    fn get_better(self) -> Choice {
+        match self {
+            Choice::Rock => Choice::Paper,
+            Choice::Paper => Choice::Scissors,
+            Choice::Scissors => Choice::Rock,
+        }
+    }
+
+    fn get_worse(self) -> Choice {
+        self.get_better().get_better()
+    }
+
+    fn cmp(self, other: Choice) -> Ordering {
+        if self == other {
+            Ordering::Equal
+        } else if self.get_better() == other {
+            Ordering::Less
+        } else {
+            Ordering::Greater
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Round {
+    opp: Choice,
+    my: Choice,
+}
+
+impl FromStr for Round {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+
+        let opp = s.chars().next().ok_or(())?;
+        let my = s.chars().next_back().ok_or(())?;
+
+        let opp = match opp {
+            'A' => Choice::Rock,
+            'B' => Choice::Paper,
+            'C' => Choice::Scissors,
+            _ => return Err(()),
+        };
+        let my = match my {
+            'X' => Choice::Rock,
+            'Y' => Choice::Paper,
+            'Z' => Choice::Scissors,
+            _ => return Err(()),
+        };
+
+        Ok(Self { opp, my })
+    }
+}
+
+impl Round {
+    fn score(self) -> u8 {
+        let diff = match self.my.cmp(self.opp) {
+            Ordering::Less => 0,
+            Ordering::Equal => 3,
+            Ordering::Greater => 6,
+        };
+        diff + self.my.score()
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Round2 {
+    opp: Choice,
+    expected: Ordering,
+}
+
+impl FromStr for Round2 {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+
+        let opp = s.chars().next().ok_or(())?;
+        let my = s.chars().next_back().ok_or(())?;
+
+        let opp = match opp {
+            'A' => Choice::Rock,
+            'B' => Choice::Paper,
+            'C' => Choice::Scissors,
+            _ => return Err(()),
+        };
+        let expected = match my {
+            'X' => Ordering::Less,
+            'Y' => Ordering::Equal,
+            'Z' => Ordering::Greater,
+            _ => return Err(()),
+        };
+
+        Ok(Self { opp, expected })
+    }
+}
+
+impl Round2 {
+    fn score(self) -> u8 {
+        let my = self.what_is_mine();
+
+        let diff = match my.cmp(self.opp) {
+            Ordering::Less => 0,
+            Ordering::Equal => 3,
+            Ordering::Greater => 6,
+        };
+        diff + my.score()
+    }
+
+    fn what_is_mine(self) -> Choice {
+        match self.expected {
+            Ordering::Less => self.opp.get_worse(),
+            Ordering::Equal => self.opp,
+            Ordering::Greater => self.opp.get_better(),
+        }
     }
 }
--- src/y2022/day3.rs
+++ src/y2022/day3.rs
@@ -1,13 +1,129 @@
+use std::{fmt::Debug, str::FromStr};
+
+use itertools::Itertools;
+
 use crate::Solution;
 
 pub struct Day3;
 
 impl Solution for Day3 {
-    fn q1(&self, _data: &str) -> String {
-        todo!()
+    fn q1(&self, data: &str) -> String {
+        let rucksacks = parse1(data);
+        rucksacks
+            .map(|sack| sack.unique_item().0 as u64)
+            .sum::<u64>()
+            .to_string()
     }
 
-    fn q2(&self, _data: &str) -> String {
-        todo!()
+    fn q2(&self, data: &str) -> String {
+        let rucksacks = parse2(data);
+        rucksacks
+            .map(|sack| sack.unique_item().0 as u64)
+            .sum::<u64>()
+            .to_string()
+    }
+}
+
+fn parse1(s: &str) -> impl Iterator<Item = Rucksack> + '_ {
+    s.split('\n').flat_map(|s| s.parse())
+}
+
+fn parse2(s: &str) -> impl Iterator<Item = Rucksack2> + '_ {
+    s.split('\n')
+        .flat_map(|s| s.parse::<Compartment>())
+        .batching(|it| Some(Rucksack2(it.next()?, it.next()?, it.next()?)))
+}
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+struct Item(u8);
+
+impl Debug for Item {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.0 < 27 {
+            write!(f, "{}", (b'a' + self.0 - 1) as char)
+        } else {
+            write!(f, "{}", (b'A' + self.0 - 27) as char)
+        }
+    }
+}
+
+struct Compartment(Vec<Item>);
+
+impl Debug for Compartment {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for item in &self.0 {
+            write!(f, "{item:?}")?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct Rucksack(Compartment, Compartment);
+
+impl FromStr for Rucksack {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+        if s.is_empty() {
+            return Err(());
+        }
+        let (first, second) = s.split_at(s.len() / 2);
+        Ok(Rucksack(first.parse()?, second.parse()?))
+    }
+}
+
+impl FromStr for Compartment {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Compartment(s.trim().chars().map(Item::parse).collect()))
+    }
+}
+
+impl Item {
+    fn parse(c: char) -> Item {
+        Item(if ('a'..='z').contains(&c) {
+            (c as u8) - b'a' + 1
+        } else {
+            (c as u8) - b'A' + 27
+        })
+    }
+
+    fn bit_mask(self) -> u64 {
+        1 << (self.0 - 1)
+    }
+
+    fn from_bit_mask(mask: u64) -> Item {
+        Item((mask.ilog2() + 1) as _)
+    }
+}
+
+impl Compartment {
+    fn item_mask(&self) -> u64 {
+        self.0
+            .iter()
+            .map(|item| item.bit_mask())
+            .fold(0, |acc, e| acc | e)
+    }
+}
+
+impl Rucksack {
+    fn unique_item(&self) -> Item {
+        let mask1 = self.0.item_mask();
+        let mask2 = self.1.item_mask();
+        Item::from_bit_mask(mask1 & mask2)
+    }
+}
+
+struct Rucksack2(Compartment, Compartment, Compartment);
+
+impl Rucksack2 {
+    fn unique_item(&self) -> Item {
+        let mask1 = self.0.item_mask();
+        let mask2 = self.1.item_mask();
+        let mask3 = self.2.item_mask();
+        Item::from_bit_mask(mask1 & mask2 & mask3)
     }
 }
