7b49d6c9e0081b351358b57e2148e80140db351e
Nicolas Mémeint <nicomem@pm.me>
2022-12-05 20:53:00 +0100

    2022 - 05

--- src/y2022/day5.rs
+++ src/y2022/day5.rs
@@ -1,13 +1,129 @@
-use crate::Solution;
+use std::{fmt::Debug, str::FromStr};
+
+use itertools::Itertools;
+
+use crate::{utils::TryCollectArray, Solution};
 
 pub struct Day5;
 
 impl Solution for Day5 {
-    fn q1(&self, _data: &str) -> String {
-        todo!()
+    fn q1(&self, data: &str) -> String {
+        let (mut drawing, moves) = parse1(data);
+
+        for mov in moves {
+            drawing.apply1(mov);
+        }
+
+        drawing
+            .stacks
+            .into_iter()
+            .map(|stack| *stack.last().unwrap())
+            .join("")
     }
 
-    fn q2(&self, _data: &str) -> String {
-        todo!()
+    fn q2(&self, data: &str) -> String {
+        let (mut drawing, moves) = parse1(data);
+
+        for mov in moves {
+            drawing.apply2(mov);
+        }
+
+        drawing
+            .stacks
+            .into_iter()
+            .map(|stack| *stack.last().unwrap())
+            .join("")
+    }
+}
+
+fn parse1(data: &str) -> (Drawing, Vec<Move>) {
+    let (part1, part2) = data.split_once("\n\n").unwrap();
+
+    (
+        part1.parse().expect("Could not parse drawing"),
+        part2.split('\n').flat_map(|s| s.parse::<Move>()).collect(),
+    )
+}
+
+type Crate = char;
+type Stack = Vec<Crate>;
+#[derive(Debug)]
+struct Drawing {
+    stacks: Vec<Stack>,
+}
+
+impl Drawing {
+    fn apply1(&mut self, mov: Move) {
+        for _ in 0..mov.quantity {
+            let crat = self
+                .stacks
+                .get_mut(mov.from as usize - 1)
+                .unwrap()
+                .pop()
+                .unwrap();
+            self.stacks.get_mut(mov.to as usize - 1).unwrap().push(crat);
+        }
+    }
+
+    fn apply2(&mut self, mov: Move) {
+        let crats: Vec<Crate> = (0..mov.quantity)
+            .map(|_| {
+                self.stacks
+                    .get_mut(mov.from as usize - 1)
+                    .unwrap()
+                    .pop()
+                    .unwrap()
+            })
+            .collect();
+        for crat in crats.into_iter().rev() {
+            self.stacks.get_mut(mov.to as usize - 1).unwrap().push(crat);
+        }
+    }
+}
+
+impl FromStr for Drawing {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let lines: Vec<_> = s.split('\n').collect();
+
+        let nb_stacks = lines.last().unwrap().split_whitespace().count();
+
+        let mut stacks: Vec<Stack> = Vec::with_capacity(nb_stacks);
+        for i_stack in 0..nb_stacks {
+            stacks.push(
+                lines
+                    .iter()
+                    .rev()
+                    .skip(1)
+                    .flat_map(|line| line.as_bytes().get(1 + (4 * i_stack)))
+                    .flat_map(|byte| char::from_u32(*byte as _))
+                    .filter(|c| !c.is_whitespace())
+                    .collect(),
+            );
+        }
+
+        Ok(Drawing { stacks })
+    }
+}
+
+#[derive(Debug)]
+struct Move {
+    quantity: u8,
+    from: u8,
+    to: u8,
+}
+
+impl FromStr for Move {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let [quantity, from, to]: [u8; 3] = s
+            .split_whitespace()
+            .flat_map(|s| s.parse::<u8>().ok())
+            .try_collect_array()
+            .ok_or(())?;
+
+        Ok(Self { quantity, from, to })
     }
 }
